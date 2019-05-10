43157e9513295802b4abd081e91ee6151b0aa668
Thaina Ribeiro <thainaribeiro.lima@outlook.com>
2019-05-09 23:13:54 -0300

    Criando tabela no banco de dados

--- Aula1AspNetMVC/Aula1AspNetMVC/Context/Aula1Context.cs
+++ Aula1AspNetMVC/Aula1AspNetMVC/Context/Aula1Context.cs
@@ -1,13 +1,15 @@
-﻿using System.Data.Entity;
+﻿using Aula1AspNetMVC.Models;
+using System.Data.Entity;
 
 namespace Aula1AspNetMVC.Context
 {
     public class Aula1Context : DbContext
     {
         public Aula1Context()
-            : base("Aula1Context")
+            : base("AulaContext")
         {
                 
         }
+        public DbSet<Cliente> Clientes { get; set; }
     }
 }
